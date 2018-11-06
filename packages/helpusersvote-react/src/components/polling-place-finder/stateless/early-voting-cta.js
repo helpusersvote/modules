14705e6eff95abbc10820fa65eb9d15480662a20
Fouad Matin <m@fouad.org>
2018-11-05 19:49:49 -0800

    add(react/ballot): call-to-action and style fixes

--- packages/helpusersvote-react/src/components/polling-place-finder/stateless/early-voting-cta.js
+++ packages/helpusersvote-react/src/components/polling-place-finder/stateless/early-voting-cta.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 export function EarlyVotingCTA({ isElectionDay, timeLeft, onClick }) {
   if (!timeLeft || isElectionDay) {
-    return null
+    return (
+      <a
+        className="db no-underline"
+        href="https://polls.vote.org/ballot/"
+        target="_blank"
+      >
+        <div className="mt3 fw5 f6 lh-copy ba blue br2 pa2 hover-bg-washed-blue pointer">
+          What&rsquo;s on your ballot?{' '}
+          <span className="dib">Find out now &rarr;</span>
+        </div>
+      </a>
+    )
   }
 
   return (
